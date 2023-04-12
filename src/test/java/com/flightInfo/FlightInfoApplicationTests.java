e232f0988b7d61c6d065266dc8d0d09bcd1358b2
Khanak09maheshwari <khanak.maheshwari104427@gmail.com>
2023-04-12 12:28:15 +0530

    initiate a project

---
+++ src/test/java/com/flightInfo/FlightInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
